ffc89f8eecbd34bbfa8cb2bcd78a16653385412f
Avinash Thadamatha <athadamatha@appriver.com>
2018-12-13 10:27:30 -0500

    First Commit

---
+++ ParkMe-master/app/src/main/java/com/example/sirangwoolheater/parkme/ParkingLot.java
@@ -0,0 +1,30 @@
+package com.example.sirangwoolheater.parkme;
+
+import java.util.Map;
+
+/**
+ * Created by sirangwoolheater on 11/9/16.
+ */
+
+public class ParkingLot {
+    private String place_id;
+    private String name;
+    private String vicinity;
+    //private String ;
+
+    public String getid(){
+        return place_id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getVicinity(){
+        return vicinity;
+    }
+
+//    public Map<String, String> getGeometry(){
+//        return geometry;
+//    }
+}
